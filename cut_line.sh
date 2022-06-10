515dc8ce51a8077b4c5f9b3e6da20951a6609328
佐藤未来 <105167202+satou-miki@users.noreply.github.com>
2022-06-11 01:46:08 +0900

    Update cut_line.sh

--- cut_line.sh
+++ cut_line.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-
 #$1のファイルの$2列目を$3に保存
 #ただし、$1のファイルはタブで区切られた2列以上のファイルに限る
-cut -f $2 $1 > $3
+
+declare -i CUT
+CUT=$2
+if [ $CUT -le $(head -n1 $1 | awk '{print NF}') ]
+then
+cut -f$CUT $1 > $3
+else
+echo "指定された列は存在しません"
+fi
